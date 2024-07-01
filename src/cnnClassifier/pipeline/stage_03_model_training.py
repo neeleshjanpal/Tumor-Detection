53f0b2533f2a24339ff7734ffa333d0a6cc2ef37
neeleshjanpal <neeleshjanpal@gmail.com>
2024-07-01 13:31:14 +0530

    model training added

--- src/cnnClassifier/pipeline/stage_03_model_training.py
+++ src/cnnClassifier/pipeline/stage_03_model_training.py
@@ -21,7 +21,7 @@ class ModelTrainingPipeline:
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<")
-        obj = Training()
+        obj = ModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==========x")
     except Exception as e:
